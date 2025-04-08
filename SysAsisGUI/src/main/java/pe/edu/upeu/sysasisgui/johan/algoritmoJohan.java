9f5231fe37fc72f199361c59c98185d1cf247cca
johanEmerson <johanirpanoca@gmail.com>
2025-04-08 14:15:10 -0500

    cambios jeia

---
+++ SysAsisGUI/src/main/java/pe/edu/upeu/sysasisgui/johan/algoritmoJohan.java
@@ -0,0 +1,7 @@
+package pe.edu.upeu.sysasisgui.johan;
+
+public class algoritmoJohan {
+    public static void main(String[] args) {
+        System.out.println("algoritmo Johan");
+    }
+}
---
+++ SysAsisGUI/src/main/java/pe/edu/upeu/sysasisgui/johan/emerson.java
@@ -0,0 +1,4 @@
+package pe.edu.upeu.sysasisgui.johan;
+
+public class emerson {
+}
