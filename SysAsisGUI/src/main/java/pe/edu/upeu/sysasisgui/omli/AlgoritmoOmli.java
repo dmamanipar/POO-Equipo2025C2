6d9c7a7343b587730a92b434b4c6c13bb28a1799
michael-lon <omarlaricoiquise31@gmail.com>
2025-04-08 09:11:08 -0500

    cambios omar larico iquise

---
+++ SysAsisGUI/src/main/java/pe/edu/upeu/sysasisgui/omli/AlgoritmoOmli.java
@@ -0,0 +1,9 @@
+package pe.edu.upeu.sysasisgui.omli;
+
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
+
+public class AlgoritmoOmli {
+    public static void main(String[] args) {
+        System.out.println("Algoritmo de Prueba");
+    }
+}
