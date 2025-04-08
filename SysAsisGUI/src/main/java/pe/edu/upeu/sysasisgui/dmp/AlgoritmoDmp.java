f583990fe080bcf0a39ddffdefeb3d6c33574432
yeins69 <abelyeins123@gmail.com>
2025-04-08 09:46:06 -0500

    cambios abf

---
+++ SysAsisGUI/src/main/java/pe/edu/upeu/sysasisgui/dmp/AlgoritmoDmp.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.sysasisgui.dmp;
+
+public class AlgoritmoDmp {
+    public static void main(String[] args) {
+        System.out.println("algoritmo de pruebaa");
+        System.out.println("ufeuef");
+    }
+}
